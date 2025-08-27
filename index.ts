84ed767880ec4545ab000eb2e59c15435befc9fd
Daniel Alexander <5389473+DAlexanderNZ@users.noreply.github.com>
2025-08-27 13:12:17 +1200

    render basic openstreetmap

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
 console.log("Hello via Bun!");
+
+Bun.serve({
+    development: true,
+    routes: {
+        "/": () => new Response(Bun.file("./public/map.html")),
+        "/about": () => new Response("About Page"),
+        "/assets/style.css": () => new Response(Bun.file("./public/assets/style.css")),
+    },
+    
+    port: 8021,
+
+    error(error) {
+        return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+            headers: { "Content-Type": "text/html" },
+        });
+    },
+});
+
+console.log(`Server is running on http://localhost:8021`);
